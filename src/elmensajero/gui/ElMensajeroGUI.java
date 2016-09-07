aeb3cea63bdbcfb11d5817b58c4e228f18dd6614
ViniciusAS <e.viniciusas@gmail.com>
2016-09-06 21:15:01 -0300

    Importações desnecessárias retiradas

--- src/elmensajero/gui/ElMensajeroGUI.java
+++ src/elmensajero/gui/ElMensajeroGUI.java
@@ -36,6 +36,7 @@ public class ElMensajeroGUI extends BorderPane {
         
         Scene scene = new Scene( this, 800, 500 );
         stage.setTitle("El Mensagero");
+        stage.setMinWidth(700);
         stage.setScene(scene);
         stage.show();
         
