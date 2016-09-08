4c6e041df973aa3fd9c07664e3358ab0e55fd356
Samuel Brat Favarin <samuelbfavarin@hotmail.com>
2016-09-08 18:15:36 -0300

    Remoção de importações inúteis

--- src/elmensajero/gui/Conversation.java
+++ src/elmensajero/gui/Conversation.java
@@ -1,26 +1,7 @@
 
 package elmensajero.gui;
 
-import elmensajero.Message;
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.paint.Color;
-import javafx.scene.effect.DropShadow;
-
 
 /**
  * classe Conversation
